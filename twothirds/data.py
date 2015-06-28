eb5cafc68465ae0bfcd883272189cf039ed157c1
Vince Knight <vincent.knight@gmail.com>
2015-06-28 17:51:38 +0100

    Tests for data
    - Adding some test data sets
    - Can import to pandas df an xls, xlsx and/or csv file
    - Tests written and pass

--- twothirds/data.py
+++ twothirds/data.py
@@ -1,4 +1,5 @@
 """A module for the data import handler"""
+import pandas
 
 class Data:
     def __init__(self, filename):
@@ -7,3 +8,16 @@ class Data:
             self.filetype = 'excel'
         elif filename.endswith('.csv'):
             self.filetype = 'csv'
+
+    def read(self):
+        if self.filetype == 'excel':
+            self._read_excel()
+        elif self.filetype == 'csv':
+            self._read_csv()
+
+    def _read_excel(self):
+        self.df = pandas.read_excel(self.filename)
+
+    def _read_csv(self):
+        self.df = pandas.read_csv(self.filename)
+        self.df.rename(columns=lambda x: x.strip(), inplace=True)
--- twothirds/tests/test_data.py
+++ twothirds/tests/test_data.py
@@ -13,3 +13,46 @@ class TestData(unittest.TestCase):
         data = twothirds.Data(file_name)
         self.assertEqual(file_name, data.filename)
         self.assertEqual('csv', data.filetype)
+
+    def test_import_from_csv_file(self):
+        file_name = 'twothirds/tests/test_data/game.csv'
+        data = twothirds.Data(file_name)
+        data.read()
+        self.assertEqual(['Name', 'Guess'], list(data.df.columns.values))
+        self.assertEqual(['A', 'B', 'C'], list(data.df['Name']))
+        self.assertEqual([1, 2, 3], list(data.df['Guess']))
+        self.assertEqual((3, 2), data.df.shape)
+
+    def test_import_from_excel_file(self):
+        file_name = 'twothirds/tests/test_data/game.xls'
+        data = twothirds.Data(file_name)
+        data.read()
+        self.assertEqual(['A', 'B', 'C'], list(data.df['Name']))
+        self.assertEqual([1, 2, 3], list(data.df['Guess']))
+        self.assertEqual((3, 2), data.df.shape)
+
+    def test_import_from_csv_file_with_no_names(self):
+        file_name = 'twothirds/tests/test_data/game1.csv'
+        data = twothirds.Data(file_name)
+        data.read()
+        self.assertEqual(['Guess'], list(data.df.columns.values))
+        self.assertEqual([1, 2, 3], list(data.df['Guess']))
+        self.assertEqual((3, 1), data.df.shape)
+
+    def test_import_from_csv_file_with_multiple_guesses(self):
+        file_name = 'twothirds/tests/test_data/game2.csv'
+        data = twothirds.Data(file_name)
+        data.read()
+        self.assertEqual(['Name', 'Guess 1', 'Guess 2'], list(data.df.columns.values))
+        self.assertEqual((3, 3), data.df.shape)
+        self.assertEqual([1, 2, 3], list(data.df['Guess 1']))
+        self.assertEqual([4, 5, 6], list(data.df['Guess 2']))
+
+    def test_import_from_excel_file_with_multiple_guesses(self):
+        file_name = 'twothirds/tests/test_data/game2.xlsx'
+        data = twothirds.Data(file_name)
+        data.read()
+        self.assertEqual(['Name', 'Guess 1', 'Guess 2'], list(data.df.columns.values))
+        self.assertEqual((3, 3), data.df.shape)
+        self.assertEqual([1, 2, 3], list(data.df['Guess 1']))
+        self.assertEqual([4, 5, 6], list(data.df['Guess 2']))
